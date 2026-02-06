f898c463991aabb6da651350266f6b3e62cfb9c1
Harispk005 <harismuhammad3333@gmail.com>
2026-02-06 11:06:04 +0530

    feat: Update Axios base URL to use production API endpoint

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://love-ring-api.vercel.app/',
     headers: {
         'Content-Type': 'application/json'
     }
