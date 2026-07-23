af11e2c47c486c3ecd35624f1472a5d12f1dda33
Aswin <aswinak1212@gmail.com>
2026-07-23 20:39:49 +0530

    Fix formatting in Axios instance configuration

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const Axios = axios.create({
     // baseURL: 'https://love-ring-api.vercel.app/',
     // baseURL:'/',
-    baseURL:'http://localhost:3000/',    
-headers: {
+    baseURL:'http://localhost:3000/',
+    headers: {
         'Content-Type': 'application/json'
     }
 });
