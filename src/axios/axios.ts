e02427e0ae16f792098773904296f4a1607683a5
Aswin <aswinak1212@gmail.com>
2026-07-28 13:31:35 +0530

    feat: add request interceptor for authorization token handling

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -9,4 +9,17 @@ const Axios = axios.create({
     }
 });
 
+Axios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default Axios;
