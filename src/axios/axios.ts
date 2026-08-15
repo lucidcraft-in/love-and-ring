2f376c722c30382ea8cfd5ebd9032ffde136cf32
Aswin <aswinak1212@gmail.com>
2026-08-15 10:45:26 +0530

    refactor: remove commented-out baseURL for cleaner axios configuration

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    // baseURL: 'https://love-ring-api.vercel.app/',
     // baseURL:'/',
     baseURL:'http://localhost:3000/',
     headers: {
